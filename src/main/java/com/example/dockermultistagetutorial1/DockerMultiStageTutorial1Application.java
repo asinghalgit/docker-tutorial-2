8c3dd97ffec6ebf94d3ea80c88aaae3eb1f02372
Anshul Singhal <anshulsinghal612@gmail.com>
2021-04-03 10:41:54 +0530

    Docker multi stage tutorial 1

---
+++ src/main/java/com/example/dockermultistagetutorial1/DockerMultiStageTutorial1Application.java
@@ -0,0 +1,13 @@
+package com.example.dockermultistagetutorial1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerMultiStageTutorial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerMultiStageTutorial1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockermultistagetutorial1/DockerMultiStageTutorial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockermultistagetutorial1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerMultiStageTutorial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
